74c28d2d3e5a12a963e696c4c3e2caf36d32552f
kolya-t <toporkov.kolya96@gmail.com>
2017-10-10 21:55:03 +0300

    New test data

--- src/main/java/ru/eninja/testwork/persons/config/DataInitializer.java
+++ src/main/java/ru/eninja/testwork/persons/config/DataInitializer.java
@@ -26,9 +26,31 @@ public class DataInitializer implements CommandLineRunner {
         Person person = new Person();
         personRepository.save(person);
 
+        // выдача паспорта и загран паспорта
         Passport passport = new Passport(person, "Иван", "Иванов", LocalDate.of(1990, Month.JULY, 23));
         Passport internationalPassport = new Passport(person, "Ivan", "Ivanov", passport.getBirthday());
-
         passportRepository.save(Arrays.asList(passport, internationalPassport));
+
+        // выдача нового паспорта взамен старого по достижении 20 лет
+        passport.invalidate();
+        Passport twentyYearsPassport = new Passport(person, passport,
+                passport.getFirstName(), passport.getLastName(), passport.getBirthday());
+        passportRepository.save(Arrays.asList(passport, twentyYearsPassport));
+
+        // смена фамилии на Петров
+        passport.invalidate();
+        internationalPassport.invalidate();
+        Passport petrovPassport = new Passport(person, passport,
+                passport.getFirstName(), "Петров", passport.getBirthday());
+        Passport petrovInternationalPassport = new Passport(person, internationalPassport,
+                internationalPassport.getFirstName(), "Petrov", internationalPassport.getBirthday());
+        passportRepository.save(Arrays.asList(
+                passport, internationalPassport, petrovPassport, petrovInternationalPassport));
+
+        // восстановление утерянного паспорта
+        petrovPassport.invalidate();
+        Passport recoveredPetrovPassport = new Passport(person, petrovPassport,
+                petrovPassport.getFirstName(), petrovPassport.getLastName(), petrovPassport.getBirthday());
+        passportRepository.save(Arrays.asList(petrovPassport, recoveredPetrovPassport));
     }
 }
